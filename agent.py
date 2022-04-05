4d4a78a67a855754523573bf7a2590ba82acee36
Armine Titizian <takkat14@joom.com>
2022-04-06 02:36:06 +0300

    fix fetch_all

--- agent.py
+++ agent.py
@@ -19,34 +19,34 @@ class OpinionAgent(Agent):
         self.mu = mu
         self.delta_x = 0
         self.delta_u = 0
-        # self.steps = 0
+        self.historic_delta_x = 0
+        self.historic_delta_u = 0
         self.regime = model_regime
+        if self.regime == "all":
+            self.mu /= self.model.num_agents
         self.is_extremist = is_extremist
 
     def fetch_p2p(self, other_agent):
-        self.delta_x = 0
-        self.delta_u = 0
-
         overlap = min(self.x + self.u, other_agent.x + other_agent.u) - max(self.x - self.u,
                                                                             other_agent.x - other_agent.u)
         if overlap > other_agent.u:
             delta_us = overlap / other_agent.u - 1
             self.delta_x += self.mu * delta_us * (other_agent.x - self.x)
             self.delta_u += self.mu * delta_us * (other_agent.u - self.u)
-        if overlap > self.u:
-            delta_other = overlap / self.u - 1
-            other_agent.delta_x += other_agent.mu * delta_other * (self.x - other_agent.x)
-            other_agent.delta_u += other_agent.mu * delta_other * (self.u - other_agent.u)
 
     def fetch_all(self):
-        # Неоптимально, потому что мозги вышли из чата
-        for agent in self.model.schedule._agents:
-            self.fetch_p2p(self.model.schedule._agents[agent])
+        for agent in self.model.schedule.agents:
+            if agent.unique_id != self.unique_id:
+                self.fetch_p2p(agent)
 
     def apply(self):
         self.x += self.delta_x
         self.x = max(self.x, -1)
         self.x = min(self.x, 1)
         self.u += self.delta_u
-        self.u = max(self.u, 1e-10)
+        self.u = max(self.u, 0)
         self.u = min(self.u, 2)
+        self.historic_delta_x = self.delta_x
+        self.historic_delta_u = self.delta_u
+        self.delta_x = 0
+        self.delta_u = 0
